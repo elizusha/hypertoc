143fde7745893e04782b5742855d369a7cc3a110
Liza Ovchinnikova <elizusha@google.com>
2020-09-16 14:58:36 +0200

    json parser added

--- demo/main.js
+++ demo/main.js
@@ -3,12 +3,11 @@ function load_player(data) {
     var list = document.getElementById("entry_list");
 
     for (entry of data["tocEntry"]) {
-        console.log(entry["url"]);
         var element = document.createElement("a");
         element.setAttribute("href", "#");
         var text = document.createTextNode(entry["name"]);
         element.appendChild(text);
-        var url = entry["url"];
+        let url = entry["url"];
         element.onclick = () => {
             player.src = url;
             player.play();
