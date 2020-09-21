f2da16ae621c1a0e5d4dfab4e42e7d2991578ca3
Liza Ovchinnikova <elizusha@google.com>
2020-09-21 14:28:57 +0200

    Audio-player for single file imlemented

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,22 @@
 function load_player(data) {
     var player = document.getElementById("player");
     var list = document.getElementById("entry_list");
-
+    list.innerHTML = "";
     for (entry of data["tocEntry"]) {
         var element = document.createElement("a");
         element.setAttribute("href", "#");
         var text = document.createTextNode(entry["name"]);
         element.appendChild(text);
         let url = entry["url"];
+        let start_offset = 0;
+        if (!("url" in entry)) {
+            url = data["assotiatedMedia"]["contentUrl"];
+            start_offset = entry["startOffset"];
+            end_offset = entry["endOffset"];
+        }
         element.onclick = () => {
             player.src = url;
+            player.currentTime = start_offset;
             player.play();
         }
         var li = document.createElement("li");
@@ -20,7 +27,14 @@ function load_player(data) {
 
 
 window.onload = (event) => {
-    fetch('./data/1411.json')
-        .then(response => response.json())
-        .then(data => load_player(data));
+    var selector = document.getElementById("audio-select");
+    selector.oninput = (event) => {
+        var source_file = event.target.value;
+        if (source_file != "") {
+            fetch(source_file)
+            .then(response => response.json())
+            .then(data => load_player(data));
+        }
+    };
 };
+
